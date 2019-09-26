2db9d13f2695867ac75651c5a185fc3ed7ba3dae
cbr600f0 <sidneybatenburg@hotmail.com>
2019-09-26 20:27:37 +0200

    begin page

--- ProjectC/MainPage.xaml.cs
+++ ProjectC/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ProjectC.Pages;
 
 namespace ProjectC
 {
@@ -16,6 +17,26 @@ namespace ProjectC
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MIJNPAGINAISWHATEVER());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
         }
     }
 }
