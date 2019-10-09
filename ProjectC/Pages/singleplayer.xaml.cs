aa25e454c8eb1b45072789bb45ff83add5961de0
cbr600f0 <sidneybatenburg@hotmail.com>
2019-10-09 13:21:41 +0200

    start backend UICreator done, no generic values yet

--- ProjectC/Pages/singleplayer.xaml.cs
+++ ProjectC/Pages/singleplayer.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,28 +14,63 @@ namespace ProjectC.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class singleplayer : ContentPage
     {
-        public int wordRows;
+        public int wordRows = 7;
+        public int wordLength = 7;
         public singleplayer()
         {
             InitializeComponent();
-            wordRows = 7;
+            UICreater();
         }
 
         public void UICreater()
         {
-            //MiddlePart.Children.Add(new Grid()
-            //{
-            //    //RowDefinitions
-            //});
+            Grid grid = new Grid();
+            StackLayout wordContainer = new StackLayout();
+            Grid insideGrid = new Grid();
 
-            //for (int i = 0; i < wordRows; i++)
-            //{
-            //    MiddlePart.Children.Add(new Label()
-            //    {C:\Users\cbr600f0\source\repos\ProjectC\ProjectC\Pages\singleplayer.xaml.cs
-            //        Text = "Mark",
+            for (int i = 0; i < wordRows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            }
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(3, GridUnitType.Star) });
 
-            //    });
-            //}
+
+            for (int row = 0; row < grid.RowDefinitions.Count(); row++)
+            {
+                wordContainer.HorizontalOptions = LayoutOptions.CenterAndExpand;
+                grid.Children.Add(new Label() { Text = "Mark" }, 0, row);
+
+                for (int i = 0; i < wordLength; i++)
+                {
+                    insideGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                }
+
+                for (int i = 0; i < insideGrid.ColumnDefinitions.Count; i++)
+                {
+                    insideGrid.Children.Add(new Frame()
+                    {
+                        Content = new Label()
+                        {
+                            Text = "a",
+                            FontSize = 20,
+                            HorizontalOptions = LayoutOptions.CenterAndExpand
+                        },
+                        Margin = 0,
+                        Padding = 0,
+                        BorderColor = Color.Black
+                    }, i, 0);
+                }
+
+
+                wordContainer.Children.Add(insideGrid);
+                grid.Children.Add(wordContainer, 1, row);
+
+                insideGrid = new Grid();
+                wordContainer = new StackLayout();
+            }
+
+            MiddlePart.Children.Add(grid);
         }
     }
 }
