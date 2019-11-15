45d29f1a103414b6ec2d36821dfd0bbe3898f12d
Eva <0946319@hr.nl>
2019-11-15 11:53:51 +0100

    WIP Difficulty Page

--- ProjectC/Pages/DifficultySelectScreen.xaml.cs
+++ ProjectC/Pages/DifficultySelectScreen.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +13,18 @@ namespace ProjectC.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DifficultySelectScreen : ContentPage
     {
+        public ICommand NavigateCommand { get; private set; }
         public DifficultySelectScreen()
         {
             InitializeComponent();
+            NavigateCommand = new Command<Type>(
+                async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await Navigation.PushAsync(page);
+                });
+
+            BindingContext = this;
         }
 
         private async void MakkelijkButton_Clicked(object sender, EventArgs e)
@@ -32,6 +42,11 @@ namespace ProjectC.Pages
         private async void LegendarischButton_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SinglePlayerPage("legendary"));
+
+        }  
+        private async void BackButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync(true);
         }
     }
 }
