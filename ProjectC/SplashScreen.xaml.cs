af7481ffe8498fd85fd2898f6a3be7ec0d6ba55f
Djaysen <31687157+Djaysen@users.noreply.github.com>
2019-11-17 13:35:21 +0100

    SplashScreen/Nieuwe Icons
    Zo goed als af. Kan misschien wat beter gemaakt worden in de toekomst, maar dit is beste wat k kan doen momenteel.

---
+++ ProjectC/SplashScreen.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashScreen : ContentPage
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ScaleIcon();
+        }
+
+        private async void ScaleIcon()
+        {
+            // wait until the UI is present
+            await Task.Delay(300);
+
+            // animate the splash logo
+            await SplashIcon.ScaleTo(0.5, 500, Easing.CubicInOut);
+            var animationTasks = new[]{
+                SplashIcon.ScaleTo(100.0, 1000, Easing.CubicInOut),
+                SplashIcon.FadeTo(0, 700, Easing.CubicInOut)
+            };
+            await Task.WhenAll(animationTasks);
+
+            //// navigate to main page
+            Navigation.InsertPageBefore(new MainPage(), Navigation.NavigationStack[0]);
+            await Navigation.PopToRootAsync(false);
+        }
+    }
+}
