6b48c77bb3616726a0a629174fc19ef4f8cd0f3a
Eva <0946319@hr.nl>
2019-12-01 20:54:45 +0100

    Revert "Revert "Merge branch 'djaysen' into eva""
    This reverts commit f24cf38873dc594b86d838c1c560419795b4627f.

---
+++ ProjectC/Helper/WebCientTimeout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+
+namespace ProjectC.Helper
+{
+    class WebCientTimeOut : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            WebRequest w = base.GetWebRequest(uri);
+            w.Timeout = 5000;  // Timeout 5 second
+            return w;
+        }
+    }
+}
